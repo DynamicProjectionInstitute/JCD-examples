6180922d702d98aa06f5f647dd0e5f86b523b695
Martin Willner <willner@dynamicprojection.com>
2018-01-23 17:13:18 +0100

    basic functionality

--- esp8266/shutter/src/main.cpp
+++ esp8266/shutter/src/main.cpp
@@ -1,17 +1,120 @@
 #include <Arduino.h>
 #include <Ultrasonic.h>
+#include <ESP8266WiFi.h>
+#include "../lib/arduino-mqtt/src/MQTTClient.h" // to make linter happy ....
+#include <string.h>
 
+#define DEBUG true
 
 Ultrasonic ultrasonic1(14,12);
 
-void setup() {
-    Serial.begin(9600);
+const char ssid[] = "JCD-stock";
+const char pass[] = "jcd-stock";
+const char broker[]= "192.168.1.1";
+static char st[8];
+static bool ledStatus = false;
+
+WiFiClient wificlient;
+MQTTClient mqttclient;
+void messageReceived(String &, String &);
+
+void wificonnect();
+void mqttconnect();
+void lighttoggle();
+
+void lighttoggle() {
+  ledStatus = !ledStatus;
+  digitalWrite(LED_BUILTIN,ledStatus);
+  #ifdef DEBUG
+  Serial.println(".");
+  #endif
+  delay(500);
+}
+
+
+void wificonnect() {
+  #ifdef DEBUG
+  Serial.println("wificonnect");
+  #endif
+  WiFi.begin(ssid,pass);
+  while(WiFi.status() != WL_CONNECTED) {
+    lighttoggle();
+  }
+  #ifdef DEBUG
+  Serial.print("IP address:");
+  Serial.println(WiFi.localIP());
+  #endif
+  digitalWrite(LED_BUILTIN,true);
 
 }
 
+void mqttconnect() {
+
+  #ifdef DEBUG
+  Serial.print("mqttconnect ");
+  Serial.println(st);
+  #endif
+    mqttclient.begin(broker,wificlient);
+  while(!mqttclient.connect(st)) {
+    lighttoggle();
+
+  }
+  mqttclient.onMessage(messageReceived);
+  #ifdef DEBUG
+  Serial.println("mqtt connected");
+  #endif
+  digitalWrite(LED_BUILTIN,true);
+  mqttclient.subscribe("dips/+/cmd");
+}
+
+
+void setup() {
+  Serial.begin(9600);
+  sprintf(st,"%x",ESP.getChipId());
+  wificonnect();
+  mqttconnect();
+
+}
+
+
+
 void loop() {
-    Serial.print("Sensor 01: ");
-    Serial.print(ultrasonic1.distanceRead()); // Prints the distance on the default unit (centimeters)
-    Serial.println("cm");
-    delay(1000);
+  mqttclient.loop();
+  delay(10);
+if(WiFi.status() != WL_CONNECTED) {
+#ifdef DEBUG
+  Serial.println("wifi broke down....");
+#endif
+wificonnect();
+}
+
+  if (!mqttclient.connected()) {
+    #ifdef DEBUG
+      Serial.println("mqtt broke down....");
+    #endif
+
+    mqttconnect();
+  }
+std::string x="dips/";
+x+=st;
+
+mqttclient.publish(x.c_str(),String(ultrasonic1.distanceRead()));
+
+  Serial.print("Sensor 01: ");
+  Serial.print(ultrasonic1.distanceRead()); // Prints the distance on the default unit (centimeters)
+  Serial.println("cm");
+  delay(1000);
+}
+
+
+void messageReceived(String &topic, String &payload) {
+  if(topic == String("dips/")) {
+    #ifdef DEBUG
+    Serial.println("match");
+    #endif
+
+}
+#ifdef DEBUG
+  Serial.println("incoming: " + topic + " - " + payload);
+#endif
 }
