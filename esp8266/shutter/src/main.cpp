2c3f1f1829e688ab18c49a502b3dc8eb0668a235
Martin Willner <willner@dynamicprojection.com>
2018-01-24 11:16:01 +0100

    v0

--- esp8266/shutter/src/main.cpp
+++ esp8266/shutter/src/main.cpp
@@ -14,6 +14,7 @@ const char pass[] = "jcd-stock";
 const char broker[]= "192.168.1.1";
 static char st[8];
 static bool ledStatus = false;
+static bool state=false;
 
 WiFiClient wificlient;
 MQTTClient mqttclient;
@@ -32,6 +33,16 @@ void lighttoggle() {
   delay(500);
 }
 
+void lightflash() {
+  digitalWrite(LED_BUILTIN,true);
+  for(int i=0;i<10;i++) {
+    ledStatus = !ledStatus;
+    digitalWrite(LED_BUILTIN,ledStatus);
+    delay(25);
+  }
+  digitalWrite(LED_BUILTIN,true);
+
+}
 
 void wificonnect() {
   #ifdef DEBUG
@@ -55,7 +66,7 @@ void mqttconnect() {
   Serial.print("mqttconnect ");
   Serial.println(st);
   #endif
-    mqttclient.begin(broker,wificlient);
+  mqttclient.begin(broker,wificlient);
   while(!mqttclient.connect(st)) {
     lighttoggle();
 
@@ -71,7 +82,10 @@ void mqttconnect() {
 
 void setup() {
   Serial.begin(9600);
-EEPROM.begin(512);
+  EEPROM.begin(512);
+  pinMode(LED_BUILTIN,OUTPUT);
+  digitalWrite(LED_BUILTIN,false);
+
   sprintf(st,"%x",ESP.getChipId());
   wificonnect();
   mqttconnect();
@@ -81,44 +95,77 @@ EEPROM.begin(512);
 }
 
 
+float measure() {
+  int x=0;
+  for(int i=0;i<=5;i++) {
+    x+=ultrasonic1.distanceRead();
+    delay(10);
+  }
+  return x/5.0;
+}
 
 void loop() {
+
+  int measurement;
+  int eepromdata;
+  lightflash();
   mqttclient.loop();
   delay(10);
-if(WiFi.status() != WL_CONNECTED) {
-#ifdef DEBUG
-  Serial.println("wifi broke down....");
-#endif
-wificonnect();
-}
+  if(WiFi.status() != WL_CONNECTED) {
+    #ifdef DEBUG
+    Serial.println("wifi broke down....");
+    #endif
+    wificonnect();
+  }
 
   if (!mqttclient.connected()) {
     #ifdef DEBUG
-      Serial.println("mqtt broke down....");
+    Serial.println("mqtt broke down....");
     #endif
 
     mqttconnect();
   }
-std::string y;
-std::string x="dips/";
-x+=st;
-y=x+"/eeprom";
-mqttclient.publish(x.c_str(),String(ultrasonic1.distanceRead()));
-mqttclient.publish(y.c_str(),String(EEPROM.read(0)));
+  std::string y;
+std::string z;
+  std::string x="dips/";
+  x+=st;
+  y=x+"/eeprom";
+  z=x+"/event";
+  measurement=measure();
+  eepromdata=EEPROM.read(0);
 
-  Serial.print("Sensor 01: ");
-  Serial.print(ultrasonic1.distanceRead()); // Prints the distance on the default unit (centimeters)
-  Serial.println("cm");
+  mqttclient.publish(x.c_str(),String(measurement));
+  mqttclient.publish(y.c_str(),String(eepromdata));
+
+  if(measurement<eepromdata) {
+    mqttclient.publish(z.c_str(),"true");
+    if(state==false) {
+    mqttclient.publish("jcd/global/jcd/cmd","SHON");
+    state=true;
+    }
+  } else {
+    mqttclient.publish(z.c_str(),"false");
+    if(state==true) {
+    mqttclient.publish("jcd/global/jcd/cmd","SHOFF");
+    state=false;
+}
+}
+
+
+
+  //Serial.print("Sensor 01: ");
+  //Serial.print(ultrasonic1.distanceRead()); // Prints the distance on the default unit (centimeters)
+  //Serial.println("cm");
   delay(1000);
 }
 
 
 void messageReceived(String &topic, String &payload) {
 
-    #ifdef DEBUG
-    Serial.println("incoming: " + topic + " - " + payload);
-    EEPROM.write(0, payload.toInt());
-    EEPROM.commit();
+  #ifdef DEBUG
+  Serial.println("incoming: " + topic + " - " + payload);
+  EEPROM.write(0, payload.toInt());
+  EEPROM.commit();
 
-#endif
+  #endif
 }
