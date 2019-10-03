16b018d383476092ba0b92c49b25421d182f1c18
Martin Willner <willner@dynamicprojection.com>
2019-10-03 09:55:07 +0200

    python3

--- JCD-Remote-Example1.py
+++ JCD-Remote-Example1.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # Sample for remote control of the JCD
 #
 # Copyright (c) 2017 by Dynamic Projeciton Institute GmbH, Vienna, Austria
@@ -16,15 +17,15 @@ jcd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # For more commands please see the JCD manual.
 
-print "Sending ON -> Projector turns on"
-jcd.sendto("ON", (jcd_host, jcd_port))
+print("Sending ON -> Projector turns on")
+jcd.sendto("ON".encode(), (jcd_host, jcd_port))
 time.sleep(10)
-print "Sending PATTERN 7 -> Set testpattern to 7"
-jcd.sendto("TEST 7", (jcd_host, jcd_port))
+print("Sending PATTERN 7 -> Set testpattern to 7")
+jcd.sendto("TEST 7".encode(), (jcd_host, jcd_port))
 time.sleep(10)
-print "Sending OFF-> Turn projector off"
-jcd.sendto("OFF", (jcd_host, jcd_port))
-print "End of demo"
+print("Sending OFF-> Turn projector off")
+jcd.sendto("OFF".encode(), (jcd_host, jcd_port))
+print("End of demo")
 
 
 
